bcace7bb6300f207911d1061f2070610616fd25f
Simar Kareer <skareer@outlook.com>
2018-07-16 10:23:04 -0400

    Network Tables

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 'use strict';
 
+const ntClient = require('wpilib-nt-client');
+const client = new ntClient.Client()
+
+client.start((isConnected, err) => {
+    // Displays the error and the state of connection
+    console.log({ isConnected, err });
+}, 'roborio-2729.local');
+
 var firebase = require("firebase");
 firebase.initializeApp({
     "appName": "MailBot",
-//    "serviceAccount": "./service-account.json",
+    //    "serviceAccount": "./service-account.json",
     "databaseURL": "https://newagent-f4967.firebaseio.com/"
 });
 
 var ref = firebase.app().database().ref();
 
-ref.on("child_changed", function(snapshot) {
+ref.on("child_changed", function (snapshot) {
+    //client.Assign(snapshot.val(), "unsorted", false) //this bool might need to be true
     console.log(snapshot.val());
-  }, function (errorObject) {
+}, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
-  });
+});
+
